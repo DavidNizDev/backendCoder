21dbe85dd6f1b8eb8392b3bffd66159ada28c96e
David Niz <dalos58.arg@gmail.com>
2022-11-06 23:33:38 -0300

    Clase 10/11

---
+++ Clase_10_Pug&Ejs/Entregable/src/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express();
+
+let productos = [
+    /* {
+        nombre: 'Ejemplo',
+        precio: '759',
+        URL: 'https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/256/examples.png'
+    } */
+];
+
+const handlebarsConfig = {
+    defaultLayout: 'index.handlebars'
+}
+app.engine('handlebars', exphbs(handlebarsConfig))
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', '../views');
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('formulario');
+})
+
+app.get('/productos', (req, res) => {
+    res.render('productos', { productos })
+    // res.redirect('/tabla');
+})
+
+app.post('/productos', (req, res) => {
+    productos.push(req.body);
+    console.log(productos);
+    res.redirect('/');
+});
+
+
+const PORT = 8080;
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+});
+server.on('error', error => console.log(`Error en servidor ${error}`));
---
+++ Clase_11_Websockets/1/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
+app.use(express.static('public'))
+const PORT = 8080
+const connectedServer = httpServer.listen(PORT, () => {
+    console.log(`Servidor Http con Websockets escuchando en el puerto ${connectedServer.address().port}`)
+})
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
